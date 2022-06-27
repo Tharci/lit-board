a2c914c9e3814799c18d5ab3121999c5f0b8e174
Marcell Tamás Tóth <tmarcellt.2000@gmail.com>
2022-06-27 06:10:44 +0200

    Added guard for multiple instances

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 
 #include "LitBoardDriver.h"
+#include <iostream>
+
 
 int main(int argc, char* argv[])
 {
+    // Check for another instance
+    HANDLE hHandle = CreateMutex( nullptr, TRUE, "litboard" );
+    if( ERROR_ALREADY_EXISTS == GetLastError() )
+    {
+        // Another instance is running
+        std::cout << "[ERROR] Another instance of the driver is already running. Exiting application...";
+        return(1); // Exit program
+    }
+
     auto& driver = lbd::LitBoardDriver::getInstance();
     driver.run();
+
+    ReleaseMutex( hHandle );
+    CloseHandle( hHandle );
+    return( 1 );
 }
